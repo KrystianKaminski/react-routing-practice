453f19027cb413038fbd1207525ef146757c9edb
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-28 18:32:55 +0100

    Simple navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -8,6 +8,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+          <Link></Link>
+        </nav>
         <Route path="/courses" exact component={Courses} />
         <Route path="/users" exact component={Users} />
       </BrowserRouter>
